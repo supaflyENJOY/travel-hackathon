bcf0d0457e71641fc04fa0fa75d5305f57da0fb5
Arsen Petrosyan <a.petrosyan@viseven.com>
2019-02-23 22:13:55 +0200

    Fixed image compressing

--- backend/src/utils/storages/local.ts
+++ backend/src/utils/storages/local.ts
@@ -1,11 +1,11 @@
-import { copy } from 'fs-extra';
+import { copyAsync } from 'fs-extra-promise';
 import { resolve } from 'path';
 import config from '../config';
 
 export default () => {
     return {
         uploadFile(localPath, key) {
-            return copy(localPath, resolve(process.cwd(), './tmp/', config.STORAGE_FOLDER, key));
+            return copyAsync(localPath, resolve(process.cwd(), './tmp/', config.STORAGE_FOLDER, key));
         }
     }
 }
